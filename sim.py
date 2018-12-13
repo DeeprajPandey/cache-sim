a5b5ed0caf35760cb5edc03ad9da586d56a2d068
Nandini18 <36665596+Nandini18@users.noreply.github.com>
2018-12-13 20:00:32 +0530

    Read word by word in file

--- sim.py
+++ sim.py
@@ -3,4 +3,11 @@ import os
 IB = os.environ.get('INSTABASE_URI',None) is not None
 open = ib.open if IB else open
 
+with open('small.memtrace','r') as trace:
+    for line in trace:
+        for word in line.split():
+            print(word)   
+
+
+
 
