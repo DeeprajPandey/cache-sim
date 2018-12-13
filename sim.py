671e41e2d9c5ba2e30bd6580a0f3369466a32404
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-12-13 23:44:55 +0530

    Started working on L1 Cache
    The offset, index and tag are calculated based on L1

--- sim.py
+++ sim.py
@@ -23,6 +23,20 @@ with open('small.memtrace','r') as trace:
             command.append(word) if count == 0
             offset.append(word) if count == 1
 
-
+# For L1 Cache
+# Cache size = 32 kB
+# Block size = 16 B
+# Num sets = 2048
+L1_limit = 2048
+L1_tag_array = []
+L1_set_array = []
+for address in addr:
+    # 16 = 2^4. offset is 4 bits from right
+    offset = address[28:32]
+    # 2048 = 2^11. index is 11 bits from right
+    index = address[17:28]
+    # rest of the bits to left of index is the tag
+    tag = address[0:17]
+    
 
 
