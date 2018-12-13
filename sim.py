acaddd8f55a8d65e8afab8ff4c7ebda92eb5b12b
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-12-14 01:11:58 +0530

    L1 cache implementation complete
    Using itertools to iterate through 10k traces. Keeping track of the index, comparing the tag value and incrementing hit and miss accordingly.

--- sim.py
+++ sim.py
@@ -1,42 +1,67 @@
 # Required for handling files in Instabase
 import os
+import itertools
+
 IB = os.environ.get('INSTABASE_URI',None) is not None
 open = ib.open if IB else open
 
 def hex2bin(hex_v):
     base = 16
+    bits = 32
     # truncating 13 bits from left (2 for '0b' and rest to make it 32 bit)
-    return bin(int(hex_v, base))[13:]
+    return bin(int(hex_v, base))[13:].zfill(bits)
 
 def bin2dec(bin_index):
     base = 2
     return int(bin_index,base)
 
-
-command = []
+instructions = []
 offset = []
 addr = []
 with open('small.memtrace','r') as trace:
     for line in trace:
         for count, word in enumerate(line.split()):
-            print(count, word)
-            command.append(word) if count == 0
-            offset.append(word) if count == 1
-
+            if count == 0:
+                instructions.append(word)
+            if count == 1:
+                offset.append(word)
+            if count == 2:
+                addr.append(hex2bin(word))
+hits = 0
+misses = 0
 # For L1 Cache
 # Cache size = 32 kB
 # Block size = 16 B
 # Num sets = 2048
 L1_limit = 2048
-L1_tag_array = []
-L1_set_array = []
-for address in addr:
+L1_tag_array = [-1]*L1_limit
+L1_set_array = [-1]*L1_limit
+
+# Using itertools to go through the instruction and address lists in parallel
+# and wrapping them with enumerate to keep track of the index
+for indx, (instruction, address) in enumerate(itertools.izip(instructions, addr)):
     # 16 = 2^4. offset is 4 bits from right
-    offset = address[28:32]
+    adr_offset = address[28:32]
     # 2048 = 2^11. index is 11 bits from right
-    index = address[17:28]
+    i = address[17:28]
+    adr_index = bin2dec(i)
     # rest of the bits to left of index is the tag
-    tag = address[0:17]
+    adr_tag = address[0:17]
     
-
-
+    # When the instruction is load
+    if instruction == 'L':
+        if L1_tag_array[adr_index] == adr_tag:
+            hits += 1
+        else:
+            # increment misses
+            misses += 1
+    if instruction == 'S':
+        if L1_tag_array[adr_index] == adr_tag:
+            hits += 1
+        else:
+            misses += 1
+            # add the tag to that index to simulate the data being added to block in set array
+            L1_tag_array[adr_index] = adr_tag
+            L1_set_array[adr_index] = bin2dec(address)
+    
+    
