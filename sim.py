2285944527d1e0e1b37213a1e6dd34581b13fbee
Nandini18 <36665596+Nandini18@users.noreply.github.com>
2018-12-13 20:35:10 +0530

    creating three arrays for command, offset and 32 bit binary address.

--- sim.py
+++ sim.py
@@ -3,10 +3,21 @@ import os
 IB = os.environ.get('INSTABASE_URI',None) is not None
 open = ib.open if IB else open
 
+def hex2bin(hex_v):
+    base = 16
+    # truncating 13 bits from left (2 for '0b' and rest to make it 32 bit)
+    return bin(int(hex_v, base))[13:]
+
+
+command = []
+offset = []
+addr = []
 with open('small.memtrace','r') as trace:
     for line in trace:
-        for word in line.split():
-            print(word)   
+        for count, word in enumerate(line.split()):
+            print(count, word)
+            command.append(word) if count == 0
+            offset.append(word) if count == 1
 
 
 
